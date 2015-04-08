d1fabfd797099974533c200b258e3b77af1bafa7
XaTToN <marcel.mueller@eclipso.de>
2015-04-08 02:07:28 +0200

    Fixed typo.. stupid code completion

--- src/factory/ClassFactory.cpp
+++ src/factory/ClassFactory.cpp
@@ -16,7 +16,7 @@
 * with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <ClassFactory\detail\ClassFactory.h>
+#include <ClassFactory/detail/ClassFactory.h>
 
 ClassFactory::~ClassFactory()
 {
